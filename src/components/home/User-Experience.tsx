888bda29a0106863d7317d589dfa2487542854ec
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-11-05 11:55:06 +0530

    style: add padding to UserExperience component for improved layout

--- src/components/home/User-Experience.tsx
+++ src/components/home/User-Experience.tsx
@@ -4,7 +4,7 @@ import userExperience from "@/assets/Images/home/id-verify.jpg"
 
 export const UserExperience = () => {
   return (
-      <div className="flex justify-center items-center ">
+      <div className="flex justify-center items-center py-4 ">
           <Image src={userExperience} alt="userExperience" width={1100} height={900} className="rounded-lg" />
       </div>
   )
