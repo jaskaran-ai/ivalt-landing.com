9a09c61ceaed14a8a3083025957d128070e204f2
Rupinder-17 <rupinderrandhey90@gmail.com>
2025-11-05 12:00:14 +0530

    fix: resolve merge conflict and clean up UserExperience component layout

--- src/components/home/User-Experience.tsx
+++ src/components/home/User-Experience.tsx
@@ -4,13 +4,6 @@ import userExperience from "@/assets/images/home/id-verify.jpg";
 
 export const UserExperience = () => {
   return (
-<<<<<<< HEAD
-      <div className="flex justify-center items-center py-4 ">
-          <Image src={userExperience} alt="userExperience" width={1100} height={900} className="rounded-lg" />
-      </div>
-  )
-}
-=======
     <div className="flex justify-center items-center ">
       <Image
         src={userExperience}
@@ -22,4 +15,3 @@ export const UserExperience = () => {
     </div>
   );
 };
->>>>>>> 55206f75a7b467222fe0521e68e63ec334caef9c
